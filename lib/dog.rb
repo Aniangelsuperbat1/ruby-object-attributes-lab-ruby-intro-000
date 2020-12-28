369df85c075e386f3169d44bced08e31e0ee8756
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-28 16:51:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,27 @@
 class Dog
+def initialize(name)
+  @name = name
+end
 
-def name
+def name =(name)
   @name = name
 end
 
 def
   @name=(new_name)
   @name = new_name
+end
+
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 end
